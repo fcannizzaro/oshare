cc4b8aba9063bde77192a04aa3e7b7f49291c7f7
Francesco Cannizzaro <francisthemoderator@gmail.com>
2017-03-25 18:11:06 +0100

    fix error url

--- src/client.js
+++ src/client.js
@@ -2,6 +2,6 @@ var socket = require('./code/socket');
 var io = require('socket.io-client');
 
 module.exports = (url, shared, remote) => {
-  var client = require('socket.io-client')('http://localhost:3000');
+  var client = require('socket.io-client')(url);
   socket.configure(shared, client, remote);
 }
