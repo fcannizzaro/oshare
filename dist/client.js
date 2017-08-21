16a5c7ad8d6e26c6d38ed7b4eae066284a24ff26
Francesco Cannizzaro <francisthemoderator@gmail.com>
2017-08-21 15:39:32 +0200

    fix reconnection

--- dist/client.js
+++ dist/client.js
@@ -2,7 +2,13 @@
 
 var socket = require('./code/socket');
 
-module.exports = function (url, options, remote) {
+var config = function config(url, options, remote) {
 	var client = require('socket.io-client')(url);
 	socket.configure(options.shared || {}, client, remote, options.authorization);
+	client.on('reconnect', function () {
+		client.disconnect();
+		config(url, options, remote);
+	});
 };
+
+module.exports = config;
--- src/client.js
+++ src/client.js
@@ -1,6 +1,12 @@
 var socket = require('./code/socket');
 
-module.exports = (url, options, remote) => {
+const config = (url, options, remote) => {
 	var client = require('socket.io-client')(url);
 	socket.configure(options.shared || {}, client, remote, options.authorization);
+	client.on('reconnect', () => {
+		client.disconnect();
+		config(url, options, remote);
+	});
 }
+
+module.exports = config;
